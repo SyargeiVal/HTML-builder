e5e4d021e9a60d24c23115d8e3c73348fc8a5103
SyargeiVal <bulbaburak@gmail.com>
2023-05-03 10:56:40 +0300

    feat: cange function

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -4,12 +4,15 @@ const { stdin, stdout } = process;
 const output = fs.createWriteStream(path.join(__dirname, 'destination.txt'));
 stdout.write('Write something to write to the file or write "exit" to exit the program.\n');
 stdin.on('data', data => {
-    const input = data.toString();  
-    process.on('SIGINT', () => process.exit());
+    const input = data.toString();
     if( input.trim() === 'exit'){
-        process.exit();
+        handle();
     } else {
         output.write(data);
     }
 });
-process.on('exit', () => stdout.write('Goodby!\n'));
+function handle() {
+    stdout.write('\nGoodby!\n');
+    process.exit();
+}
+process.on('SIGINT', handle);
